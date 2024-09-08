8461f118c8577def30691b7cfe7452a0bc4cc74c
mts755Dev <muhammadtalhasaleem.755@gmail.com>
2024-09-08 15:09:26 -0400

    fix: footer updated

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,7 +5,6 @@ function Footer() {
   return (
     <footer className="footer-section">
       <div className="footer-section-container">
-        <p>Website developed by <a href="https://www.muhammadtalhasaleem.com/" target='_blank' rel="noreferrer">mts</a><span> &copy;</span></p>
       </div>
     </footer>
   );
